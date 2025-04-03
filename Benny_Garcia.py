fc26b2136f7ab7f8d22a684ae5c42151a71b5035
Benny <bgarcia13@cocc.edu>
2025-04-02 17:08:29 -0700

    Added rect_area function

---
+++ Benny_Garcia.py
@@ -0,0 +1,9 @@
+# Benny Garcia
+#CS 162 Spring 2025
+# Week 1
+# Git Assignment
+
+# Function 1: Computes the Area of a Rectangle
+def rect_area(length, width):
+    """Returns the area of a rectangle given length and width."""
+    return length * width
