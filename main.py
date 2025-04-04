c89636e938211797c7db852b018e7e688fdfad32
KatieFournier1 <61601784+KatieFournier1@users.noreply.github.com>
2025-04-04 16:13:14 -0700

    specify function written by katie

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def rect_area(length, width):
     return length * width
 #Function 2
 # Returns Surface Area of Rectangular Solid
+# Written by Katie Fournier
 def rect_surface_area(length: int, width: int, height: int) -> int:
     return 2 * (rect_area(length, width) + rect_area(length, height) + rect_area(width, height))
 
